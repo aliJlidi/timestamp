34dd54d37f3067b60115db9adcecd67ced5715b2
Tuni Games <ali_jlidi85@yahoo.com>
2019-12-31 22:26:21 +0100

    changing code and adding new file

--- app.js
+++ app.js
@@ -1,58 +1,20 @@
-const express = require("express");
-const bodyParser = require("body-parser")
-const cors = require("cors")
+const time = require('./time');
+const path = require('path');
+const express = require('express');
 const app = express();
-app.use(cors())
-app.use(bodyParser.urlencoded({extended:true}));
+const PORT = process.env.PORT || 8080;
 
 
-//Get call to return JSON that formats natural and unix date
 
-app.get("/timestamp/:dateVal",(req, res, next)=>{
-    //gets the data from the use endPoint
-    var dateVal = req.params.dateVal;
-    // formatting date in natural date view
-    var dateFormatting ={
-        year: 'numeric',
-        month:'long',
-        day: 'numeric'
-    };
-    if(isNaN(dateVal)){
-        var naturalDate = new Date(dateVal);
-        naturalDate = naturalDate.toLocaleDateString("en-us",dateFormatting);
-        var unixDate = new Date(dateVal).getTime()/1000;
-    }else{
-        var naturalDate = new Date(dateVal*1000);
-        var unixDate = dateVal;
-        naturalDate = naturalDate.toLocaleDateString("en-us",dateFormatting);
-    }
-    res.json({unix:unixDate, natural: naturalDate});
+app.get('/api/timestamp/:input', (req,res) => {
+  try {
+    res.send(time(req.params.input));
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(400);
+  }
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+app.listen(PORT, () => {
+  console.log(`App running on port: ${PORT}`);
 });
