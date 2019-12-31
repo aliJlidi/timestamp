68cbea30c7e66b533fd4f4d17c415e6f96e10e6a
Tuni Games <ali_jlidi85@yahoo.com>
2019-12-31 22:41:15 +0100

    add the main root

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const time = require('./time');
 const path = require('path');
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
-
+app.get('/',(req, res)=>{
+   res.send("<div><a href='/api/timestamp/2015-12-25'>Click here</a></div>") 
+});
 
 app.get('/api/timestamp/:input', (req,res) => {
   try {
